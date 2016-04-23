d5784c3fa42a2e0737ae29fa0004c781e4386220
Vitaliy <23neyron@gmail.com>
2016-04-23 07:47:01 +0300

    add reply to cimments #16

--- comments/models.py
+++ comments/models.py
@@ -5,10 +5,14 @@ from django.contrib.contenttypes.models import ContentType
 
 
 class CommentManager(models.Manager):
+	def all(self):
+		qs = super(CommentManager, self).filter(parent=None)
+		return qs
+
 	def filter_by_instance(self,instance):
 		content_type = ContentType.objects.get_for_model(instance.__class__)
 		obj_id = instance.id
-		qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id)
+		qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id).filter(parent=None)
 		return qs
 
 
@@ -17,11 +21,24 @@ class Comment(models.Model):
 	content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
 	object_id = models.PositiveIntegerField()
 	content_object = GenericForeignKey('content_type', 'object_id')
+	parent = models.ForeignKey('self', null=True, blank=True)
 
 	content = models.TextField()
 	timestamp = models.DateTimeField(auto_now_add=True)
 
 	objects = CommentManager()
 
+	class Meta:
+		ordering = ['-timestamp']
+
 	def __str__(self):
 		return self.user.username
+
+	def children(self): #ответы
+		return Comment.objects.filter(parent=self)
+
+	@property
+	def is_parent(self):
+		if self.parent is not None:
+			return False
+		return True
--- posts/views.py
+++ posts/views.py
@@ -22,14 +22,15 @@ def post_create(request):
 		instance = form.save(commit=False)
 		instance.user = request.user
 		instance.save()
+		# message success
 		messages.success(request, "Successfully Created")
 		return HttpResponseRedirect(instance.get_absolute_url())
 	context = {
-		"form":form,
+		"form": form,
 	}
-	return	render(request, "post_form.html", context)
+	return render(request, "post_form.html", context)
 
-def post_detail(request, slug=None): #RETRIEVE
+def post_detail(request, slug=None):
 	instance = get_object_or_404(Post, slug=slug)
 	if instance.publish > timezone.now().date() or instance.draft:
 		if not request.user.is_staff or not request.user.is_superuser:
@@ -38,47 +39,61 @@ def post_detail(request, slug=None): #RETRIEVE
 
 	initial_data = {
 			"content_type": instance.get_content_type,
-			"object_id": instance.id,
+			"object_id": instance.id
 	}
 	form = CommentForm(request.POST or None, initial=initial_data)
 	if form.is_valid():
-		c_type = form.cleaned_data.get('content_type')
+		c_type = form.cleaned_data.get("content_type")
 		content_type = ContentType.objects.get(model=c_type)
 		obj_id = form.cleaned_data.get('object_id')
-		content_data = form.cleaned_data.get('content')
+		content_data = form.cleaned_data.get("content")
+		parent_obj = None
+		try:
+			parent_id = int(request.POST.get("parent_id"))
+		except:
+			parent_id = None
+
+		if parent_id:
+			parent_qs = Comment.objects.filter(id=parent_id)
+			if parent_qs.exists() and parent_qs.count() == 1:
+				parent_obj = parent_qs.first()
+
+
 		new_comment, created = Comment.objects.get_or_create(
 							user = request.user,
-							content_type = content_type,
+							content_type= content_type,
 							object_id = obj_id,
-							content = content_data
-		)
+							content = content_data,
+							parent = parent_obj,
+						)
+		return HttpResponseRedirect(new_comment.content_object.get_absolute_url())
+
 
 	comments = instance.comments
-
 	context = {
 		"title": instance.title,
 		"instance": instance,
 		"share_string": share_string,
-		"comments":comments,
+		"comments": comments,
 		"comment_form":form,
 	}
-	return	render(request, "post_detail.html",context)
+	return render(request, "post_detail.html", context)
 
-def post_list(request): #list items
+def post_list(request):
 	today = timezone.now().date()
-	queryset_list = Post.objects.all()#filter(draft=False).filter(publish__lte=timezone.now())#.order_by("-timestamp")
+	queryset_list = Post.objects.active() #.order_by("-timestamp")
 	if request.user.is_staff or request.user.is_superuser:
 		queryset_list = Post.objects.all()
 
 	query = request.GET.get("q")
 	if query:
 		queryset_list = queryset_list.filter(
-		Q(title__icontains=query)|
-		Q(content__icontains=query)|
-		Q(user__first_name__icontains=query)|
-		Q(user__last_name__icontains=query)
-		).distinct()
-	paginator = Paginator(queryset_list, 3) # Show 25 contacts per page
+				Q(title__icontains=query)|
+				Q(content__icontains=query)|
+				Q(user__first_name__icontains=query) |
+				Q(user__last_name__icontains=query)
+				).distinct()
+	paginator = Paginator(queryset_list, 8) # Show 25 contacts per page
 	page_request_var = "page"
 	page = request.GET.get(page_request_var)
 	try:
@@ -89,14 +104,16 @@ def post_list(request): #list items
 	except EmptyPage:
 		# If page is out of range (e.g. 9999), deliver last page of results.
 		queryset = paginator.page(paginator.num_pages)
-	context = {
-			"object_list": queryset,
-			"title": "List",
-			'page_request_var': page_request_var,
-			'today': today,
-		}
 
-	return	render(request, "post_list.html",context)
+
+	context = {
+		"object_list": queryset,
+		"title": "List",
+		"page_request_var": page_request_var,
+		"today": today,
+	}
+	return render(request, "post_list.html", context)
+
 
 
 
@@ -104,25 +121,27 @@ def post_list(request): #list items
 def post_update(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
-	instance = get_object_or_404(Post, slug=slug )
+	instance = get_object_or_404(Post, slug=slug)
 	form = PostForm(request.POST or None, request.FILES or None, instance=instance)
 	if form.is_valid():
 		instance = form.save(commit=False)
 		instance.save()
 		messages.success(request, "<a href='#'>Item</a> Saved", extra_tags='html_safe')
 		return HttpResponseRedirect(instance.get_absolute_url())
+
 	context = {
 		"title": instance.title,
 		"instance": instance,
-		"form": form,
+		"form":form,
 	}
 	return render(request, "post_form.html", context)
+
 
 
 def post_delete(request, slug=None):
 	if not request.user.is_staff or not request.user.is_superuser:
 		raise Http404
-	instance = get_object_or_404(Post, slug=slug )
+	instance = get_object_or_404(Post, slug=slug)
 	instance.delete()
 	messages.success(request, "Successfully deleted")
 	return redirect("posts:list")
