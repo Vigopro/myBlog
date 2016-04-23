f9f14260c4b11f24b8b2a6fb2e0c493b0dba79fb
Vitaliy <23neyron@gmail.com>
2016-04-23 21:23:40 +0300

    I conducted a GLOBAL FIX ALL BUGS #17,5

--- posts/views.py
+++ posts/views.py
@@ -1,17 +1,19 @@
-from django.db.models import Q
+
 from urllib.parse import quote_plus
 from django.contrib import messages
+from django.contrib.contenttypes.models import ContentType
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from django.db.models import Q
 from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.utils import timezone
-# Create your views here.
 
+from comments.forms import CommentForm
+from comments.models import Comment
 from .forms import PostForm
 from .models import Post
-from comments.models import Comment
-from comments.forms import CommentForm
-from django.contrib.contenttypes.models import ContentType
+
 
 def post_create(request):
 	if not request.user.is_staff or not request.user.is_superuser:
