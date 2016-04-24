eeab5cb2ae70470bf73e4996afaa014220c74b02
Vitaliy <23neyron@gmail.com>
2016-04-24 17:41:33 +0300

    add read time in django #20

--- posts/models.py
+++ posts/models.py
@@ -9,6 +9,7 @@ from markdown_deux import markdown
 from django.utils.safestring import mark_safe
 from comments.models import Comment
 from django.contrib.contenttypes.models import ContentType
+from .utils import get_read_time
 # Create your models here.
 #MVC MODEL VIEW CONTROLLER
 
@@ -37,6 +38,7 @@ class Post(models.Model):
 	content = models.TextField()
 	draft = models.BooleanField(default=False)
 	publish = models.DateField(auto_now=False, auto_now_add=False)
+	read_time = models.TimeField(null=True, blank=True)
 	updated = models.DateTimeField(auto_now=True, auto_now_add=False)
 	timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
 
@@ -87,5 +89,9 @@ def pre_save_post_receiver(sender, instance, *args, **kwargs):
 	if not instance.slug:
 		instance.slug = create_slug(instance)
 
+	if instance.content:
+		html_string = instance.get_markdown()
+		read_time_var = get_read_time(html_string)
+		instance.read_time = read_time_var
 
 pre_save.connect(pre_save_post_receiver, sender=Post)
